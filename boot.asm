45c923cf8226b6f898cf07f39931245750bf18e6
sanchana-arun <sanchanaarun21@gmail.com>
2025-06-12 12:06:13 +0000

    learning how to use the stack

--- boot.asm
+++ boot.asm
@@ -82,6 +82,8 @@ int 0x10
 mov al, 'S'
 int 0x10
 
+mov al, 13
+int 0x10
 mov al, 10
 int 0x10
 
@@ -121,6 +123,36 @@ int 0x10
 ; mov al, [0x7c2d]
 ; int 0x10
 
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+mov al, [0x7ffe]
+int 0x10
+
+mov al, [0x7ffc]
+int 0x10
+
+; mov al, [0x8000]
+; int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10 
+
+pop bx
+mov al, bl
+int 0x10
+
+mov al, [0x8000]
+int 0x10
 
 jmp $
 
