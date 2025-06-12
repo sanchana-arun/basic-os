31ef9666bbd3607b0af32e6784ad4cbb957ed794
sanchana-arun <sanchanaarun21@gmail.com>
2025-06-12 12:34:36 +0000

    some cleaning up

--- boot.asm
+++ boot.asm
@@ -1,5 +1,7 @@
 ; [org 0x7c00]
 mov ah, 0x0e 
+
+; prints "Hello, This is Sanchana's OS"
 mov al, 'H'
 int 0x10
 
@@ -123,36 +125,36 @@ int 0x10
 ; mov al, [0x7c2d]
 ; int 0x10
 
-mov bp, 0x8000
-mov sp, bp
+; mov bp, 0x8000
+; mov sp, bp
 
-push 'A'
-push 'B'
-push 'C'
+; push 'A'
+; push 'B'
+; push 'C'
 
-mov al, [0x7ffe]
-int 0x10
+; mov al, [0x7ffe]
+; int 0x10
 
-mov al, [0x7ffc]
-int 0x10
+; mov al, [0x7ffc]
+; int 0x10
+
+; ; mov al, [0x8000]
+; ; int 0x10
+
+; pop bx
+; mov al, bl
+; int 0x10
+
+; pop bx
+; mov al, bl
+; int 0x10 
+
+; pop bx
+; mov al, bl
+; int 0x10
 
 ; mov al, [0x8000]
 ; int 0x10
-
-pop bx
-mov al, bl
-int 0x10
-
-pop bx
-mov al, bl
-int 0x10 
-
-pop bx
-mov al, bl
-int 0x10
-
-mov al, [0x8000]
-int 0x10
 
 jmp $
 
