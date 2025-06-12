722243955c8aa7c6579bf8be6b4d150d248178a1
sanchana-arun <sanchanaarun21@gmail.com>
2025-06-12 11:53:25 +0000

    added gitignore

--- boot.asm
+++ boot.asm
@@ -85,47 +85,47 @@ int 0x10
 mov al, 10
 int 0x10
 
-;attempting to print the the_secret
-;attempt 1
-mov al, "1"
-int 0x10
-mov al, the_secret
-int 0x10
+; ;attempting to print the the_secret
+; ;attempt 1
+; mov al, "1"
+; int 0x10
+; mov al, the_secret
+; int 0x10
 
-mov al, 10
-int 0x10
+; mov al, 10
+; int 0x10
 
-;attempt 2
-mov al, "2"
-int 0x10
-mov al, [the_secret]
-int 0x10
+; ;attempt 2
+; mov al, "2"
+; int 0x10
+; mov al, [the_secret]
+; int 0x10
 
-mov al, 10
-int 0x10
+; mov al, 10
+; int 0x10
 
-;attempt 3
-mov al, "3"
-int 0x10
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
+; ;attempt 3
+; mov al, "3"
+; int 0x10
+; mov bx, the_secret
+; add bx, 0x7c00
+; mov al, [bx]
+; int 0x10
 
-mov al, 10
-int 0x10
+; mov al, 10
+; int 0x10
 
-;attempt 4
-mov al, "4"
-int 0x10
-mov al, [0x7c2d]
-int 0x10
+; ;attempt 4
+; mov al, "4"
+; int 0x10
+; mov al, [0x7c2d]
+; int 0x10
 
 
 jmp $
 
-the_secret:
-    db "X"
+; the_secret:
+;     db "X"w
 
 times 510-($-$$) db 0
 dw 0xaa55
