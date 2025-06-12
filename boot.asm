3d52a8bd581be2e421e017a3e66025e4290e9055
sanchana-arun <sanchanaarun21@gmail.com>
2025-06-12 08:57:41 +0000

    printing 'hello, this is sanchana'

--- boot.asm
+++ boot.asm
@@ -1,5 +1,50 @@
-loop:
-    jmp loop
+mov ah, 0x0e 
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 
+mov al, 'o'
+int 0x10
+mov al, ','
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'T'
+int 0x10
+mov al, 'h'
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 's'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 's'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'c'
+int 0x10
+mov al, 'h'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'a'
+int 0x10
 
+jmp $
 times 510-($-$$) db 0
 dw 0xaa55
