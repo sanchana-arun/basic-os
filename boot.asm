ac81264b46b6b0c27ddb419e0f3cf1e17c7c07da
sanchana-arun <sanchanaarun21@gmail.com>
2025-06-12 11:44:29 +0000

    trying to understand bootsector memory

--- boot.asm
+++ boot.asm
@@ -1,50 +1,131 @@
+; [org 0x7c00]
 mov ah, 0x0e 
 mov al, 'H'
 int 0x10
+
 mov al, 'e'
 int 0x10
+
 mov al, 'l'
 int 0x10
-int 0x10 
+int 0x10
+
 mov al, 'o'
 int 0x10
+
 mov al, ','
 int 0x10
+
 mov al, ' '
 int 0x10
+
 mov al, 'T'
 int 0x10
+
 mov al, 'h'
 int 0x10
+
 mov al, 'i'
 int 0x10
+
 mov al, 's'
 int 0x10
+
 mov al, ' '
 int 0x10
+
 mov al, 'i'
 int 0x10
+
 mov al, 's'
 int 0x10
+
 mov al, ' '
 int 0x10
+
 mov al, 'S'
 int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, 'c'
-int 0x10
-mov al, 'h'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'n'
-int 0x10
+
 mov al, 'a'
 int 0x10
 
+mov al, 'n'
+int 0x10
+
+mov al, 'c'
+int 0x10
+
+mov al, 'h'
+int 0x10
+
+mov al, 'a'
+int 0x10
+
+mov al, 'n'
+int 0x10
+
+mov al, 'a'
+int 0x10
+
+mov al, 39
+int 0x10
+
+mov al, 's'
+int 0x10
+
+mov al, ' '
+int 0x10
+
+mov al, 'O'
+int 0x10
+
+mov al, 'S'
+int 0x10
+
+mov al, 10
+int 0x10
+
+;attempting to print the the_secret
+;attempt 1
+mov al, "1"
+int 0x10
+mov al, the_secret
+int 0x10
+
+mov al, 10
+int 0x10
+
+;attempt 2
+mov al, "2"
+int 0x10
+mov al, [the_secret]
+int 0x10
+
+mov al, 10
+int 0x10
+
+;attempt 3
+mov al, "3"
+int 0x10
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+mov al, 10
+int 0x10
+
+;attempt 4
+mov al, "4"
+int 0x10
+mov al, [0x7c2d]
+int 0x10
+
+
 jmp $
+
+the_secret:
+    db "X"
+
 times 510-($-$$) db 0
 dw 0xaa55
