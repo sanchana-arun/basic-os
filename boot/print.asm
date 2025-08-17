2f04ddf31d3b750c470068fa3971942b08dc2e58
Sanchana Arun <130022933+sanchana-arun@users.noreply.github.com>
2025-08-17 20:02:51 +0530

    Update print.asm

--- boot/print.asm
+++ boot/print.asm
@@ -1,20 +1,20 @@
 print:
     pusha
 
-; keep this in mind:
-; while (string[i] != 0) { print string[i]; i++ }
+ 
+ 
 
-; the comparison for string end (null byte)
+ 
 start:
-    mov al, [bx] ; 'bx' is the base address for the string
+    mov al, [bx]  
     cmp al, 0 
     je done
 
-    ; the part where we print with the BIOS help
+     
     mov ah, 0x0e
-    int 0x10 ; 'al' already contains the char
+    int 0x10  
 
-    ; increment pointer and do next loop
+     
     add bx, 1
     jmp start
 
@@ -28,9 +28,9 @@ print_nl:
     pusha
     
     mov ah, 0x0e
-    mov al, 0x0a ; newline char
+    mov al, 0x0a  
     int 0x10
-    mov al, 0x0d ; carriage return
+    mov al, 0x0d  
     int 0x10
     
     popa
