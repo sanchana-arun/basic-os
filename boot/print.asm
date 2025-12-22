e98a826b18cd9ca2bb9301f6586cdd9730cfdc9c
Sanchana Arun <130022933+sanchana-arun@users.noreply.github.com>
2025-12-22 14:00:11 +0530

    p
    Removed unnecessary blank lines in print.asm.

--- boot/print.asm
+++ boot/print.asm
@@ -1,28 +1,20 @@
 print:
     pusha
 
- 
- 
-
- 
 start:
     mov al, [bx]  
     cmp al, 0 
     je done
-
      
     mov ah, 0x0e
     int 0x10  
 
-     
     add bx, 1
     jmp start
 
 done:
     popa
     ret
-
-
 
 print_nl:
     pusha
